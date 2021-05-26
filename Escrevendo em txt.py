be3135c654f4caff4f16d1b3afd08ed314e636a6
Mateus de Almeida <contato.mat55@gmail.com>
2021-05-26 11:35:30 -0300

    Delete Escrevendo em txt.py

--- Escrevendo em txt.py
+++
@@ -1,8 +0,0 @@
-import random
-from random import randint
-var1 = randint(000, 999)
-number = (str(var1))
-#CRIANDO E ESCREVENDO EM TXT
-arquivo = open("numeros.txt", "a")
-arquivo.write('\n # {}'.format (number))
-print(var1)
